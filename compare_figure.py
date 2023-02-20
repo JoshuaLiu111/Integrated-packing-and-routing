5fc9467d962b482f9244d95f6dbb9ab70c92a7c5
Dr.Wombat <liucongzheng@joshuas-mbp.home>
2023-02-20 20:41:35 +0000

    hist

--- compare_figure.py
+++ compare_figure.py
@@ -5,7 +5,6 @@ Created on Mon Feb 20 15:34:39 2023
 
 @author: Joshua Liu
 """
-from typing import Dict
 import json
 import numpy as np
 import plotly.graph_objects as go
@@ -13,60 +12,97 @@ import plotly.io as io
 io.renderers.default='svg'
 
 
-def dict2figure_compare(C_dict:Dict):
+class figure_compare():
     
+    def __init__(self,path:str):
+        #read experiment data
+        with open(path) as json_file:
+            output_full = json.load(json_file)
+          
+        out_ = output_full['whether3d_True']['class_num_8']['box_num_10']
+        
+        #get best
+        Temp_num = len(out_[list(out_)[0]][0])
+        for des in out_:
+            read = out_[des]
+            man = []
+            for i in range(Temp_num):
+                man.append(min(read[2][i],read[3][i],read[4][i],read[5][i],read[6][i]))
+            out_[des] = [read[0],read[1],man]
+        self.out_ = out_
+        
+    def dict2figure_hist(self):
+        rate = [[],[]]
+        for des in self.out_:
+            base = self.out_[des][0]
+            spr = self.out_[des][1]
+            comp = self.out_[des][2]
+            for i in range(len(base)):
+                rate[0].append((spr[i]-base[i])/base[i])
+                rate[1].append((comp[i]-base[i])/base[i])
+        
+        color = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 180, 3)]
+        figure_nume = 'Compare outputs with 10 boxes_hist'
+        
+        fig = go.Figure()
+        fig.add_trace(go.Box(y=rate[0],boxpoints='all', name='SPR',marker_color = color[1]))
+        fig.add_trace(go.Box(y=rate[1],boxpoints='all', name='MAN',marker_color = color[2]))
+        
+        fig.update_layout(
+            xaxis = dict(tickfont = dict(size=25)),
+            yaxis = dict(title='Accuracy loss',
+                         tickfont = dict(size=25),
+                         titlefont = dict(size = 25))
+        )
+        
+        fig.write_image('Figures/' + figure_nume + '.png')
 
-    Temp_num = len(C_dict[list(C_dict)[0]][0])
-    Fun_num = 3
-    
-    color = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 180, Fun_num)]
-    fun_name = ['EXT','SPR','MAN']
-    
-    figure_nume = 'Compare outputs with 10 boxes'
-    
-    x = np.concatenate([([i]*Temp_num) for i,j in sorted(C_dict.items())], axis=0).tolist()
+    def dict2figure_compare(self):
+        
 
-    fig = go.Figure()
+        Temp_num = len(self.out_[list(self.out_)[0]][0])
+        Fun_num = 3
+        
+        color = ['hsl('+str(h)+',50%'+',50%)' for h in np.linspace(0, 180, Fun_num)]
+        fun_name = ['EXT','SPR','MAN']
+        
+        figure_nume = 'Compare outputs with 10 boxes'
+        
+        x = np.concatenate([([i]*Temp_num) for i,j in sorted(self.out_.items())], axis=0).tolist()
 
-    for i in range(Fun_num):    
-        combine_perform = []
-        for j in list(C_dict):
-            combine_perform += C_dict[j][i]
-        fig.add_trace(go.Box(
-            y = combine_perform,
-            x = x,
-            name = fun_name[i],
-            marker_color = color[i]
-        ))
-    
+        fig = go.Figure()
 
-    fig.update_layout(
-        xaxis = dict(tickfont = dict(size=25)),
-        yaxis = dict(title='Total cost',
-                     tickfont = dict(size=25),
-                     titlefont = dict(size = 25)),
-        legend = dict(font = dict(size = 25)),
-        boxmode='group' # group together boxes of the different traces for each value of x
-    )
-    
-    
-    fig.write_image('Figures/' + figure_nume + '.png')
+        for i in range(Fun_num):    
+            combine_perform = []
+            for j in list(self.out_):
+                combine_perform += self.out_[j][i]
+            fig.add_trace(go.Box(
+                y = combine_perform,
+                x = x,
+                name = fun_name[i],
+                marker_color = color[i]
+            ))
+        
+
+        fig.update_layout(
+            xaxis = dict(tickfont = dict(size=25)),
+            yaxis = dict(title='Total cost',
+                         tickfont = dict(size=25),
+                         titlefont = dict(size = 25)),
+            legend = dict(font = dict(size = 25)),
+            boxmode='group' # group together boxes of the different traces for each value of x
+        )
+        
+        
+        fig.write_image('Figures/' + figure_nume + '.png')
     
 
 if __name__ == "__main__":
     
-    #read experiment data
-    with open('compare_output.json') as json_file:
-        output_full = json.load(json_file)
-      
-    out_ = output_full['whether3d_True']['class_num_8']['box_num_10']
+    path = 'compare_output.json'
     
-    #get best
-    Temp_num = len(out_[list(out_)[0]][0])
-    for des in out_:
-        read = out_[des]
-        man = []
-        for i in range(Temp_num):
-            man.append(min(read[2][i],read[3][i],read[4][i],read[5][i],read[6][i]))
-        out_[des] = [read[0],read[1],man]
-    dict2figure_compare(out_)
+    dict_set = figure_compare(path)
+    dict_set.dict2figure_hist()
+    dict_set.dict2figure_compare()
+    
+    
